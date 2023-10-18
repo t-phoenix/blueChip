7db5f9c23a15db4a62462f568cf8518f40b3c71f
Abhinil Agarwal <abhinil@Abhinils-MacBook-Pro.local>
2023-10-18 20:09:46 +0530

    editing data alignment

--- web-client/src/components/learn/IndexFund.js
+++ web-client/src/components/learn/IndexFund.js
@@ -6,7 +6,7 @@ export default function IndexFund(){
     return(
         <div className="content-container">
             <h2>What the hell is Index Fund?</h2>
-            <p>Index fund is like a mutual fund or Exchange traded fund, which is designed to follow certain preset rules so that it can replicate the performance of a specified crypto basket of underlying investment.</p>
+            <p className="left-para">Index fund is like a mutual fund or Exchange traded fund, which is designed to follow certain preset rules so that it can replicate the performance of a specified crypto basket of underlying investment.</p>
         </div>
     )
 }
